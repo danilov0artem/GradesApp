5938208b882584434f15019ea5259308801c3a9e
artem danilov <artem@artems-MacBook-Air.local>
2025-07-29 01:27:00 +0400

    Add readInt function

--- main.cpp
+++ main.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 
-int readInt(const std::string& prompt = "Enter an integer: ") {
+int readInt(int min, int max, const std::string& prompt = "Enter an integer: ") {
     while (true) {
         std::cout << prompt;
         std::string line;
@@ -13,11 +13,14 @@ int readInt(const std::string& prompt = "Enter an integer: ") {
             size_t pos;
             int value = std::stoi(line, &pos);
             if (pos != line.size()) {
-                // Extra characters after number
                 throw std::invalid_argument("Trailing characters");
             }
+            if (value < min || value > max) {
+                std::cout << "Please enter a number between " << min << " and " << max << ".\n";
+                continue;
+            }
             return value;
-        } catch (...) {
+        } catch (const std::exception&) {
             std::cout << "Invalid input. Please enter a valid integer.\n";
         }
     }
@@ -35,45 +38,49 @@ enum class MenuItem {
 };
 
 void menu() {
-    std::cout << "1. Load grades\n"; 
-    std::cout << "2. Save grades\n"; 
-    std::cout << "3. Show grades\n";
-    std::cout << "4. GPA\n"; 
-    std::cout << "5. Add grade\n"; 
-    std::cout << "6. Change grade\n"; 
-    std::cout << "7. Delete grade\n"; 
-    std::cout << "0. Exit\n";
+    std::vector<int> grades;
 
-    std::cout << ">>> ";
-    std::string menuChoice;
-    getline(std::cin, menuChoice);
-    
-    if (menuChoice == MenuItem::loadGrades) {
-        
-    }
-    else if (menuChoice == MenuItem::saveGrades) {
+    while (true) {
+        std::cout << "1. Load grades\n"; 
+        std::cout << "2. Save grades\n"; 
+        std::cout << "3. Show grades\n";
+        std::cout << "4. GPA\n"; 
+        std::cout << "5. Add grade\n"; 
+        std::cout << "6. Change grade\n"; 
+        std::cout << "7. Delete grade\n"; 
+        std::cout << "0. Exit\n";
 
-    }
-    else if (menuChoice == MenuItem::showGrades) {
+        std::cout << ">>> ";
+        std::string menuChoice;
+        getline(std::cin, menuChoice);
         
-    }
-    else if (menuChoice == MenuItem::GPA) {
-        
-    }
-    else if (menuChoice == MenuItem::addGrade) {
-        
-    }
-    else if (menuChoice == MenuItem::changeGrade) {
-        
-    }
-    else if (menuChoice == MenuItem::deleteGrade) {
-        
-    }
-    else if (menuChoice == MenuItem::exit) {
-        
-    }
-    else {
-        std::cout << "Invalid input! Press any key to continue...\n";
+        if (menuChoice == MenuItem::loadGrades) {
+            
+        }
+        else if (menuChoice == MenuItem::saveGrades) {
+
+        }
+        else if (menuChoice == MenuItem::showGrades) {
+            
+        }
+        else if (menuChoice == MenuItem::GPA) {
+            
+        }
+        else if (menuChoice == MenuItem::addGrade) {
+            
+        }
+        else if (menuChoice == MenuItem::changeGrade) {
+            
+        }
+        else if (menuChoice == MenuItem::deleteGrade) {
+            
+        }
+        else if (menuChoice == MenuItem::exit) {
+            
+        }
+        else {
+            std::cout << "Invalid input! Press any key to continue...\n";
+        }
     }
 }
 
